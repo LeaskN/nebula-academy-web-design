921ed569cdf0fc468b805a4204ce15bd6165a7fb
Nicholas Leask <leask.nicholas@gmail.com>
2019-12-18 16:57:07 +0545

    resized bottom icons

--- software-engineering/src/components/Global/Footer.js
+++ software-engineering/src/components/Global/Footer.js
@@ -9,8 +9,8 @@ import './Footer.css';
 
 export const Footer = () => (
     <React.Fragment>
-        <Navbar bg="dark" className="mainNavbar navbar-dark navbar-collapse" toggleable expand="lg">
-            <Navbar.Brand  href="/"><img src={NebulaIcon} height="125px" alt=""></img></Navbar.Brand>
+        <Navbar bg="dark" className="footerNavbar mainNavbar navbar-dark navbar-collapse" toggleable expand="lg">
+            <Navbar.Brand  href="/"><img src={NebulaIcon} height="100px" alt=""></img></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <ul>
             <Nav.Item><Nav.Link href="/">About</Nav.Link></Nav.Item>
@@ -21,10 +21,10 @@ export const Footer = () => (
             </ul>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="https://twitter.com/nynebula?lang=en"><img src={TwitterIcon} height="125px" alt=""></img></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.facebook.com/NebulaAcademyNY/"><img src={FacebookIcon} height="125px" alt=""></img></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.instagram.com/nebulaacademia/"><img src={InstagramIcon} height="125px" alt=""></img></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.linkedin.com/company/40795558/admin/"><img src={LinkedInIcon} height="125px" alt=""></img></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://twitter.com/nynebula?lang=en"><img src={TwitterIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://www.facebook.com/NebulaAcademyNY/"><img src={FacebookIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://www.instagram.com/nebulaacademia/"><img src={InstagramIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://www.linkedin.com/company/40795558/admin/"><img src={LinkedInIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
