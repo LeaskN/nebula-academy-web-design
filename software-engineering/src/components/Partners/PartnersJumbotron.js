ee1e1a8ff7362d850289a8659e45ca65036d9780
Nicholas Leask <leask.nicholas@gmail.com>
2020-01-06 13:27:45 -0500

    added partners jumbo background

--- software-engineering/src/components/Partners/PartnersJumbotron.js
+++ software-engineering/src/components/Partners/PartnersJumbotron.js
@@ -4,8 +4,7 @@ import './PartnersJumbotron.css'
 
 export const PartnersJumbotron = () => (
   <React.Fragment>
-    <Jumbotron className="PartnersJumbotron">
-      <div className="partnersOverlay"></div>
+    <Jumbotron className="partnersJumbotron">
       <Container>
         <h1 className="jumboText">Partners</h1>
       </Container>
