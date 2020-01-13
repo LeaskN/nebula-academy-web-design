dafd54a00395b7ea1b1314b7599fb6da16592de3
Nicholas Leask <leask.nicholas@gmail.com>
2020-01-13 16:02:50 -0500

    university jumbotron picture and writing updated

--- AWS/src/components/Universities/UniversitiesJumbotron.js
+++ AWS/src/components/Universities/UniversitiesJumbotron.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container } from 'react-bootstrap';
+import { Jumbotron, Container, Button } from 'react-bootstrap';
 import './UniversitiesJumbotron.css'
 
 export const UniversitiesJumbotron = () => (
@@ -8,6 +8,9 @@ export const UniversitiesJumbotron = () => (
       <div className="universitiesOverlay"></div>
       <Container>
         <h1 className="jumboText">Universities</h1>
+        <p>Keeping up with the ever changing and growing workforce needs by industry is challenging, colleges and universities know this all too well.</p>
+        <p>Nebula Academy supports building capacity by bridging the gap between academia and industry. </p>
+        <Button style={{zIndex:"100"}} href="https://calendly.com/tomlemons/nebulaacademy" target="empty" variant="success">Impact My University Today</Button>
       </Container>
     </Jumbotron>
   </React.Fragment>
