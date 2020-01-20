722d99c1c3205fd344f75b7d5bce83a6f4203642
Nicholas Leask <leask.nicholas@gmail.com>
2020-01-20 15:15:12 -0500

    added Tom to staff

--- AWS/src/components/Staff/StaffContent.js
+++ AWS/src/components/Staff/StaffContent.js
@@ -6,6 +6,7 @@ import NicLeaskImage from "../../assets/NicLeaskPHOTO.jpg"
 import MichaelCareyImage from "../../assets/MCNebulaAcademy.jpg"
 import FrancineMaioneImage from "../../assets/FTMNebulaAcademy.jpg"
 import JuleneReedImage from "../../assets/DrJuleneReedNebulaAcademy.jpg"
+import TomLemonsImage from "../../assets/TomLemonsImage.jpg"
 
 class StaffContent extends Component {
     constructor(props) {
@@ -20,6 +21,8 @@ class StaffContent extends Component {
                 { name: 'Michael Carey', refName:'MichaelCarey', position:'STEAM Coach & AWS Software Architect', linkText: `Michael's LinkedIn`, url:'https://www.linkedin.com/in/michael-carey-6a162b7/', image: MichaelCareyImage, bio: 'As CEO of IT Systems Group, Mr. Carey was very early to market during the proliferation of the file server and the explosion of networking in the corporate and government environments. As Long Island’s first Certified Novell Engineer, he quickly built a successful company centered around sales and support of new and emerging, game-changing technologies. With an intense passion and a deep understanding of how technology helps businesses reach their goals, Mr. Carey built the platforms from which his sales and operation staff succeeded. Through instruction and coaching, he empowers his people to be creators of their own processes and their own successes. Besides building sales and operational teams, Mr. Carey used his programming skills to develop customized business reporting systems for the purposes of supplying his teams with highly relevant, actionable, real-time management information. Most recently, Mr. Carey’s builds mobile and web solutions for business leveraging today’s cloud platforms including Amazon Web Services and Microsoft Azure. Mr. Carey holds a Master of Science in Electrical Engineering and a Bachelor of Engineering with a double major in Electrical Engineering and Computer Science from the State University of New York at Stony Brook.'},
                 { name: 'Francine Maione', refName:'FrancineMaione', position:'Instructional Technology Consultant', linkText: `Francine's LinkedIn`, url:'https://www.linkedin.com/in/francine-touliatos-maione-16924813b/', image: FrancineMaioneImage, bio: 'Francine Maione is an Instructional Technology Consultant currently specializing in integrating STEM Equipment into Pre-K, Primary, Middle and Secondary Classrooms.  She graduated from New York University with a degree in Educational Theatre. Francine entered the field of Technology as a Teacher Assistant/Computer Specialist in a K-6 Elementary School in Manhasset, NY. Her experiences working with both students and teachers in a Computer Lab has fueled her passion to assist todays Educators in gaining the skills necessary to encourage creativity & innovation in a STEM setting. She is a Microsoft Innovative Educator (MIE) and enjoys volunteering for We Connect The Dots, Inc.'},
                 { name: 'Dr. Julene Reed', refName:'DrJuleneReed', position:'Professional Development Consultant', linkText: `Julene's LinkedIn`, url:'https://www.linkedin.com/in/julenereed/', image: JuleneReedImage, bio: 'Julene is a professional development consultant, researcher, and educator with over twenty–five years of experience in education. Julene holds a doctorate in education leadership with a focus on global education. She is an Apple Distinguished Educator, Apple Teacher, Google for Education Certified Trainer and Innovator, Google Earth Education Expert, Microsoft Innovative Educator and Trainer, education author, and international consultant and presenter.'}, 
+                { name: 'Tom Lemons', refName:'TomLemons', position:'Business Development', linkText: `Tom's Linkedin`, url:'https://www.linkedin.com/in/thomas-lemons-206196a2/', image: TomLemonsImage, bio: 'Tom is a graduate of Rollins College with a bachelor in business and social entrepreneurship. Tom is a catalyst to our growth in the North East of the US. Tom comes from a family of educators across Massachusetts. He is following in their footsteps by modernizing what a current higher education looks like.'}, 
+                { name: 'Tom Lemons', refName:'TomLemons', position:'Business Development', linkText: `Tom's Linkedin`, url:'https://www.linkedin.com/in/thomas-lemons-206196a2/', image: TomLemonsImage, bio: 'Tom is a graduate of Rollins College with a bachelor in business and social entrepreneurship. Tom is a catalyst to our growth in the North East of the US. Tom comes from a family of educators across Massachusetts. He is following in their footsteps by modernizing what a current higher education looks like.'}, 
             ],            
         };
         this.clickedEle = this.clickedEle.bind(this);
@@ -90,9 +93,7 @@ class StaffContent extends Component {
                             <br/>
                             <h2 className="centralText pointer">{person.name}</h2>
                             <h4 className="greyText centralText pointer">{person.position}</h4>
-                            <a className="link centralText" href={person.url} target="empty">
-                                {person.linkText}
-                            </a>
+                            <a className="link centralText" href={person.url} target="empty">{person.linkText}</a>
                             <hr/>
                         </Col> 
                     </React.Fragment>
