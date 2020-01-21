108daa56768bcdf0fd2d5f590dcd92039908ce96
Nicholas Leask <leask.nicholas@gmail.com>
2020-01-21 18:49:19 -0500

    removed bio

--- software-engineering/src/App.js
+++ software-engineering/src/App.js
@@ -14,6 +14,7 @@ import { Partners } from './pages/Partners';
 import { GuestSpeakers } from './pages/GuestSpeakers';
 import { HiddenApplication } from './pages/HiddenApplication';
 import { TryCoding } from './pages/TryCoding';
+import { Staff } from './pages/Staff';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
             <Route path ="/Partners" component={Partners}/>
             <Route path ="/GuestSpeakers" component={GuestSpeakers}/>
             <Route path ="/TryCoding" component={TryCoding}/>
+            <Route path ="/staff" component={Staff}/>
             <Route path ="/HiddenApplication" component={HiddenApplication}/>
             <Route component = {NoMatch}/>
             {/*
