41a7dc307aaa971ad70eb5f31a7336c9ec2926d2
Nicholas Leask <leask.nicholas@gmail.com>
2020-01-22 16:59:21 -0500

    formatted footer

--- AWS/src/components/Global/Footer.js
+++ AWS/src/components/Global/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar  } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import FacebookIcon from '../../assets/FacebookIcon.png';
 import InstagramIcon from '../../assets/InstagramIcon.png';
 import TwitterIcon from '../../assets/TwitterIcon.png';
@@ -10,22 +10,24 @@ import './Footer.css';
 export const Footer = () => (
     <React.Fragment>
         <Navbar bg="dark" className="footerNavbar mainNavbar navbar-dark navbar-collapse" toggleable="true" expand="lg">
-            <Navbar.Brand href="/"><img src={NebulaIcon} height="100px" alt=""></img></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <ul>
-            <Nav.Item><Nav.Link href="/">About</Nav.Link></Nav.Item>
-            {/* <Nav.Item><Nav.Link href="/careers">Careers</Nav.Link></Nav.Item> */}
-            <Nav.Item><Nav.Link href="/staff">Staff</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/universities">Universities</Nav.Link></Nav.Item>
-            </ul>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="https://twitter.com/nynebula?lang=en"><img src={TwitterIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.facebook.com/NebulaAcademyNY/"><img src={FacebookIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.instagram.com/nebulaacademia/"><img src={InstagramIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.linkedin.com/company/40795558/admin/"><img src={LinkedInIcon} height="100px" alt=""></img></Nav.Link></Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
+            <Container>
+                <Navbar.Brand href="/"><img src={NebulaIcon} height="70px" alt=""></img></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <ul>
+                    <li className="noPadding noMargin smallLineHeight"><Nav.Link href="/">About</Nav.Link></li>
+                    {/* <li className="noPadding noMargin smallLineHeight"><Nav.Link href="/careers">Careers</Nav.Link></li> */}
+                    <li className="noPadding noMargin smallLineHeight"><Nav.Link href="/staff">Staff</Nav.Link></li>
+                    <li className="noPadding noMargin smallLineHeight"><Nav.Link href="/universities">Universities</Nav.Link></li>
+                </ul>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Item><Nav.Link href="https://twitter.com/nynebula?lang=en"><img src={TwitterIcon} height="70px" alt=""></img></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href="https://www.facebook.com/NebulaAcademyNY/"><img src={FacebookIcon} height="70px" alt=""></img></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href="https://www.instagram.com/nebulaacademia/"><img src={InstagramIcon} height="70px" alt=""></img></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href="https://www.linkedin.com/company/40795558/admin/"><img src={LinkedInIcon} height="70px" alt=""></img></Nav.Link></Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     </React.Fragment>
 )
