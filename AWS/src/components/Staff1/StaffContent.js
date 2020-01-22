8bc4a2bf1cba67bb085e68e8a702c5047115fabb
Nicholas Leask <leask.nicholas@gmail.com>
2020-01-22 16:51:30 -0500

    updated staff page

--- AWS/src/components/Staff1/StaffContent.js
+++ AWS/src/components/Staff1/StaffContent.js
@@ -1,9 +1,5 @@
 import React, { Component }from 'react';
-import { Row, Col, Container, Image} from 'react-bootstrap';
-import Modal from 'react-modal';
-import SlidingPane from 'react-sliding-pane';
-import 'react-sliding-pane/dist/react-sliding-pane.css';
-
+import { Row, Col, Container, Image } from 'react-bootstrap';
 import './StaffContent.css';
 import LaurieCareyImage from "../../assets/LaurieNebulaAcademy.jpg"
 import NicLeaskImage from "../../assets/NicLeaskPHOTO.jpg"
@@ -16,15 +12,9 @@ class StaffContent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: false,
-            setShow: false,
             viewWidth:0,
             colWidth: 294,
-            name:'',
-            activeBio:'',
-            activePosition:'',
-            isPaneOpen: false,
-            isPaneOpenLeft: false,
+            name:"",
             allPeople: [ 
                 { name: 'Laurie Carey', refName:'LaurieCarey', position:'CEO & Education Management Consultant', linkText: `Book Laurie's Time`, url:'https://app.acuityscheduling.com/schedule.php?owner=13648189&appointmentType=category:Request%20appointment%20for%20services%20with%20Laurie%20Carey%20', image: LaurieCareyImage, bio:'Laurie Carey is a new American educator – challenging the problems of American education with a multi-prong vision that brings the tools of business success, innovative educational practices, and strategic technology to schools, districts, universities, teachers, students and parents. Laurie Carey is not the proprietor of another educational product or service.  The entities that she has founded, Nebula Academy, Laurie Carey Consulting, LLC and We Connect The Dots, Inc., are driven by ideas, passions, individual skills, partnerships and the efficacy of research. Both entities utilize mutually supporting designs to bring American education to a new level of effectiveness and leadership by replacing stagnation and irrelevance with passionate motivational learning and teaching.'}, 
                 { name: 'Nic Leask', refName:'NicLeask', position:'STEAM Coach & Software Engineer', linkText: `Nic's LinkedIn`, url:'https://www.linkedin.com/in/nicholas-leask/', image: NicLeaskImage, bio: 'Nic is a STEAM Coach & Software Engineer. He is a Microsoft Innovative Educator (MIE) Certified Instructor and Microsoft: Education Educator (MEE) Certified Instructor.  He attended the Minecraft: Education Edition training at the Microsoft Corporate Office in Redmond, Washington. He graduated from Stony Brook University where he attained a degree in both Environmental Studies(BA) and Business Management(BS). Post-graduation Nic worked for an environmental marketing agency and later worked at a technology recruitment firm. The latter is where Nic found his opportunity to move into technology. He attended a software engineering bootcamp in NYC where he gained experience working primarily with React, JavaScript, Node.js, HTML, & CSS. Nic has helped shape Nebula Academy, taught the importance of bringing tech into the classroom and how to utilize technology as a resource tool to engage students in immersive learning experiences.'},
@@ -34,13 +24,12 @@ class StaffContent extends Component {
                 { name: 'Tom Lemons', refName:'TomLemons', position:'Business Development', linkText: `Tom's Linkedin`, url:'https://www.linkedin.com/in/thomas-lemons-206196a2/', image: TomLemonsImage, bio: 'Tom is a graduate of Rollins College with a bachelor in business and social entrepreneurship. Tom is a catalyst to our growth in the North East of the US. Tom comes from a family of educators across Massachusetts. He is following in their footsteps by modernizing what a current higher education looks like.'}, 
             ],            
         };
-        this.setInfo = this.setInfo.bind(this);
+        this.clickedEle = this.clickedEle.bind(this);
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
     componentDidMount() {
         this.updateWindowDimensions();
         window.addEventListener('resize', this.updateWindowDimensions);
-        Modal.setAppElement(this.el);
     }
     
     componentWillUnmount() {
@@ -53,80 +42,63 @@ class StaffContent extends Component {
         let string = JSON.stringify(bio);
         return string;
     }
-
-    setInfo(event){
+    showBio(name){
+        for (let i = 0; i < this.state.allPeople.length; i++) {
+            let person = this.state.allPeople[i];
+            if (person.refName === name){
+                return <Col sm={12} className={this.state.name !== person.refName ? "hidden" : "showing"}><br/>{(person.bio)}</Col>
+            }
+        }
+    }
+    clickedEle(event){
         let target = event.target;
         if (target.parentElement.className === 'col'){
             let person = target.parentElement.childNodes[2].innerHTML.split(' ').join('').split('.').join('');
-            if (this.state.name !== person) {
-                for (let i = 0; i < this.state.allPeople.length; i++) {
-                    const individual = this.state.allPeople[i];
-                    if(person === individual.refName)
-                    this.setState({
-                        name: person,
-                        activeBio: individual.bio,
-                        activePosition: individual.position
-                    })
-                } 
+            if (this.state.name === person){
+                this.setState({
+                    name: ''
+                })
+            } else {
+                this.setState({
+                    name: person
+                })
             }
         } else if (target.parentElement.parentElement.className === 'col'){
             let person = target.parentElement.parentElement.childNodes[2].innerHTML.split(' ').join('').split('.').join('');
-
-            if (this.state.name !== person) {
-                for (let i = 0; i < this.state.allPeople.length; i++) {
-                    const individual = this.state.allPeople[i];
-                    if(person === individual.refName)
-                    this.setState({
-                        name: person,
-                        activeBio: individual.bio,
-                        activePosition: individual.position
-                    })
-                } 
+            if (this.state.name === person){
+                this.setState({
+                    name: ''
+                })
+            } else {
+                this.setState({
+                    name: person
+                })
             }
         } 
     }
-    setInfoOpenPanel(e){
-        this.setInfo(e);
-        this.setState({ isPaneOpen: true });
-    }
+
     render() {
         return (
-            <div ref={ref => this.el = ref}>
-                <SlidingPane
-                    className='some-custom-class slidingPane'
-                    overlayClassName='some-custom-overlay-class'
-                    isOpen={ this.state.isPaneOpen }
-                    title={ this.state.name }
-                    subtitle={ this.state.activePosition }
-                    from='bottom'
-                    width='100vw'
-                    onRequestClose={ () => {
-                        this.setState({ isPaneOpen: false });
-                    } }>
-                    <div>{this.state.activeBio}</div>
-                    <br />
-                </SlidingPane>
-                <React.Fragment>
-                    <Container>
-                        <Row>{this.state.allPeople.map(person =>
-                            <React.Fragment>
-                                <Col >
-                                    <div className="imageContainer" >
-                                        <Image src={person.image} fluid />
-                                    </div>
-                                    <br/>
-                                    <h2 className="centralText">{person.name}</h2>
-                                    <h4 className="centralText">{person.position}</h4>
-                                    <h6 className="centralText linkDark" onClick={(e) => this.setInfoOpenPanel(e)}>About {person.name.split(' ').length === 3? person.name.split(' ')[0] + ' ' + person.name.split(' ')[2] : person.name.split(' ')[0]}</h6>
-                                    <a className="link centralText" href={person.url} target="empty">{person.linkText}</a>
-                                    <br/>
-                                </Col> 
-                            </React.Fragment>
-                        )}</Row>
-                        
-                    </Container>     
-                </React.Fragment> 
-            </div>
+            <Container>
+                {/* <Row>{this.state.allPeople.map(person =>
+                    <Col sm={12} className={this.state.name !== person.refName ? "hidden" : "showing "}><br/>{this.showBio(this.state.name)}</Col>
+                )}</Row> */}
+                <Row>{this.state.allPeople.map(person =>
+                    <React.Fragment>
+                        <Col onClick={(e) => this.clickedEle(e)}>
+                            <div className="imageContainer" >
+                                <Image src={person.image} fluid />
+                            </div>
+                            <br/>
+                            <h2 className="centralText">{person.name}</h2>
+                            <h4 className="greyText centralText">{person.position}</h4>
+                            <a className="link centralText" href={person.url} target="empty">{person.linkText}</a>
+                            <hr/>
+                        </Col> 
+                    </React.Fragment>
+                )}</Row>
+                
+            </Container>      
         )
     }
 }
