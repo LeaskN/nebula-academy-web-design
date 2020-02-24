671897bd2cbafad845ddebdeef08aede2a73976e
Nicholas Leask <leask.nicholas@gmail.com>
2020-02-24 11:40:41 -0500

    updated scholarship page

--- CSTeach/src/components/Scholarships/ScholarshipsFooter.js
+++ CSTeach/src/components/Scholarships/ScholarshipsFooter.js
@@ -6,8 +6,6 @@ export const ScholarshipsFooter = () => (
   <Container className="fullScholarshipsFooter">
     <Jumbotron>
       <h1 className="contentCentered">Build your teaching capacity with Nebula Academy</h1><br/>
-      
-      <p>
         <Row>
           <Col className="contentCentered">
             <Button href="/curriculum" variant="primary">Curriculum</Button>
@@ -16,7 +14,6 @@ export const ScholarshipsFooter = () => (
             <Button href="/certifications" variant="primary">Apply</Button>
           </Col>
         </Row>
-      </p>
     </Jumbotron>
   </Container>
 )
