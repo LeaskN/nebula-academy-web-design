5beac83d162583e56af534a079e4bfe223ce510a
rosendopili <rosendopili@gmail.com>
2020-05-20 17:05:53 -0400

    adding original linkedin-card edits

--- software-engineering/src/components/GuestSpeakers/GuestSpeakersContent.js
+++ software-engineering/src/components/GuestSpeakers/GuestSpeakersContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Container } from 'react-bootstrap';
+import { Row, Col, Card, Button, Container } from 'react-bootstrap';
 import './GuestSpeakersContent.css'
 
 
