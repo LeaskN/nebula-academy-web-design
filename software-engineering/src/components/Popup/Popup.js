11b30cce0dcf7805a68756f7e0affe9f1bfcc5be
rosendopili <rosendopili@gmail.com>
2020-07-30 23:16:00 -0400

    added popup message devops 426

---
+++ software-engineering/src/components/Popup/Popup.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Modal, Button } from 'react-bootstrap';
+import './Popup.css'; 
+
+
+function Popup() {
+  const [show, setShow] = useState(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <>
+      <Modal className="popupModal" show={show} onHide={handleClose}>
+        <Modal.Header className="popupHeader" closeButton>
+          <Modal.Title className="popupTitle">New Courses!</Modal.Title>
+        </Modal.Header>
+        <p className="popupText" >3 Month <span>Full-Time</span> or 6 Month <span>Part-Time</span></p>
+        <p className="popupText" >Cohorts Beginning <span>September 14th</span>!</p>
+        <Modal.Footer className="popupFooter">
+            <Button variant="secondary" href="/program">
+                Course Details
+            </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Popup;
--- software-engineering/src/pages/Home.js
+++ software-engineering/src/pages/Home.js
@@ -3,11 +3,13 @@ import { HomeJumbotron } from '../components/Home/HomeJumbotron';
 import SectionNav from '../components/Global/SectionNav';
 import HomeContent from '../components/Home/HomeContent';
 import  { HomeFooter } from '../components/Home/HomeFooter';
+import Popup from '../components/Popup/Popup'; 
 
 
 export const Home = () => (
     <React.Fragment>
         <HomeJumbotron/>
+        <Popup/> 
         <SectionNav/>
         <HomeContent/>
         <HomeFooter/>
