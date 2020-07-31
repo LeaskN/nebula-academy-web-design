5a9ea05f3e6c3e6f909e6e6a13b8d0ac9f7fd83c
Nicholas Leask <leask.nicholas@gmail.com>
2020-07-31 15:22:57 -0400

    added guest speakers button under internship description, fixed text-wrap in footer

--- software-engineering/src/components/Internships/InternshipsContent.js
+++ software-engineering/src/components/Internships/InternshipsContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button} from 'react-bootstrap';
 import './InternshipsContent.css'
 
 
@@ -23,11 +23,12 @@ class InternshipsContent extends Component {
                     </Col>
                     <Col xs={1}></Col>
                 </Row>
+                <Row style={{ display:'flex', justifyContent: 'center',  }}>
+                    <Button href="/guestspeakers">View Guest Speakers</Button>
+                </Row>
                 <Row>
                     <Col xs={1}></Col>
                     <Col xs={10}>
-                        {/* <h1>What Sponsors Say:</h1>
-                        <p> THIS TEXT NEEDS TO BE CHANGED: The primary focus of this program is to have participants land jobs in coding & technology oriented roles. There are different paths that one can take to achieve this goal but the most consistent is through internship opportunities. We are partnered with many local companies who are looking for software engineering interns. These roles frequently lead to job placement within that company or open up new avenues towards full time employment elsewhere.</p> */}
                         <h1>Why Are Internships Important?</h1>
                         <p>An internship enables participants to gain first-hand exposure of working in the real world. It also allows participants the ability to harness the skill, knowledge, and lab practice they learnt in our program. Internships provide the opportunity for participants to demonstrate the value they can contribute to employers immediately after completion of our program.  We guarantee each participant in our program an internship opportunity so that they can build the confidence and build their portfolio for gaining employment.</p>
                     </Col>
