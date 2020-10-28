ea680681071557f4dd1ceb7dfed7204304650c8b
tefedrb-enterprise <tefebell@gmail.com>
2020-10-28 15:46:29 -0400

    broke down createTextForPage() into smaller methods & adjusted popup.js

--- software-engineering/src/components/Popup/Popup.js
+++ software-engineering/src/components/Popup/Popup.js
@@ -22,46 +22,44 @@ function Popup() {
     }
   }, [show]);
 
-  const renderCohortDetails = () => {
-    try{
-      return buildCohortDetails()
+  const cohortDetailsJSX = () => {
+    try {
+      return filterBootCamps().map((cohort, key) => formatDetailsIntoJSX(cohort, key));
     } catch (e){
       console.log("Error caught: " + e);
     }
   }
 
-  const buildCohortDetails = () => {
-    return programs.map((cohort, key) => {
-      if(cohort.isActive && cohort.name[0] === "V"){
-        const [duration, fullOrPartTime, startDate] = parseCohortStr(cohort);
-        return (
-          <p key={key} className="popupText">
-            {duration.toLowerCase().split(' ').join('-')}
-            <span> {fullOrPartTime.toLowerCase().split(' ').join('-')} </span>
-            cohort begins 
-            <span> {startDate}</span>
-          </p>
-        )
-      }
-      return null
-    });
+  const filterBootCamps = () => {
+    return programs.filter(option => option.name.toLowerCase().includes('bootcamp') && option.isActive);
+  }
+  
+  const formatDetailsIntoJSX = (cohort, key) => {
+    const [duration, fullOrPartTime, startDate] = parseCohortStr(cohort);
+    return (
+      <p key={key} className="popupText">
+        {duration.toLowerCase().split(' ').join('-')}
+        <span> {fullOrPartTime.toLowerCase().split(' ').join('-')} </span>
+        cohort begins 
+        <span> {startDate}</span>
+      </p>
+    )
   }
 
-  function parseCohortStr(cohort){
+  const parseCohortStr = (cohort) => {
       const [duration] = /[0-9] Month/i.exec(cohort.name);
       const [fullOrPartTime] = /Full time|Part time/i.exec(cohort.name);
       const [rawStartDate] = /\w{3} \d* \d{4}/i.exec(new Date(cohort.startDate));
-      const month = createFullMonthStr(rawStartDate);
-      const day = /\d{2}|\d{1}/.exec(rawStartDate);
-      const year = /\d{4}/.exec(rawStartDate);
-      const startDate = `${month} ${day}, ${year}`;
+      const startDate = parseDateStr(rawStartDate);
       return [duration, fullOrPartTime, startDate];
   }
 
-  function createFullMonthStr(str){
+  const parseDateStr = (str) => {
     const fullMonthNames = ["January", "February", "March", "April", 
       "May", "June", "July", "August", "September", "October", "December"];
-    const startDate = fullMonthNames.filter(month => month.includes(/\w{3}/i.exec(str)[0]))[0];
+    const [, month, day, year] = /(\w{3}) (\d{2}|\d{1}) (\d{4})/.exec(str);
+    const fullMonth = fullMonthNames.filter(mo => mo.includes(month));
+    const startDate = `${fullMonth} ${day}, ${year}`;
     return startDate;
   }
 
@@ -70,7 +68,7 @@ function Popup() {
       <Modal.Header className="popupHeader" closeButton>
         <Modal.Title className="popupTitle">New Courses!</Modal.Title>
       </Modal.Header>
-      {programs ? renderCohortDetails() : <></>}
+      {programs ? cohortDetailsJSX() : <></>}
       <Modal.Footer className="popupFooter">
         <Button className="popupButton" variant="secondary" href="/program">
           Course Details
--- software-engineering/src/components/Program/ProgramDesc.js
+++ software-engineering/src/components/Program/ProgramDesc.js
@@ -7,36 +7,57 @@ class ProgramDesc extends Component {
         };
     }
     componentDidMount(){
-
       // fetch(`http://localhost:3000/dev2/campaigns`)
       return fetch(`https://d9nuj9xdv4try.cloudfront.net/dev2/campaigns`)
         .then(res => res.json())
         .then(res => this.setState({cohortOptions: res}))
         .catch(err => {console.log(err)})
-
     }
 
-    createTextForPage(){
-      let allPrograms = [];
+    // createTextForPage(){
+    //   let allPrograms = [];
+    //   if( this.state.cohortOptions ){
+    //     this.state.cohortOptions.forEach(element => {
+    //       if( element.name.toLowerCase().indexOf('bootcamp') > -1 && element.name.indexOf('6 Month') > -1){
+    //         allPrograms.push(<p key={element.name} className="currentCohortsP"><span className="currentCohortsSpan">{element.name.slice(0, 8)}, Part time session:</span> {`${new Date(element.startDate + 'EST').toDateString()}`} - {`${new Date(element.endDate + 'EST').toDateString()}`}</p>)
+    //       } else if( element.name.toLowerCase().indexOf('bootcamp') > -1 ) {
+    //         allPrograms.push(<p key={element.name} className="currentCohortsP"><span className="currentCohortsSpan">{element.name.slice(0, 8)}, Full time session:</span> {`${new Date(element.startDate + 'EST').toDateString()}`} - {`${new Date(element.endDate + 'EST').toDateString()}`}</p>)
+    //       }
+    //     })
+    //   }
+    //   return allPrograms;
+    // }
 
-      if( this.state.cohortOptions ){
-        this.state.cohortOptions.forEach(element => {
-          if( element.name.toLowerCase().indexOf('bootcamp') > -1 && element.name.indexOf('6 Month') > -1){
-            allPrograms.push(<p key={element.name} className="currentCohortsP"><span className="currentCohortsSpan">{element.name.slice(0, 8)}, Part time session:</span> {`${new Date(element.startDate + 'EST').toDateString()}`} - {`${new Date(element.endDate + 'EST').toDateString()}`}</p>)
-          } else if( element.name.toLowerCase().indexOf('bootcamp') > -1 ) {
-            allPrograms.push(<p key={element.name} className="currentCohortsP"><span className="currentCohortsSpan">{element.name.slice(0, 8)}, Full time session:</span> {`${new Date(element.startDate + 'EST').toDateString()}`} - {`${new Date(element.endDate + 'EST').toDateString()}`}</p>)
-          }
-        })
+    cohortDetailsJSX(){
+      try {
+        if(this.state.cohortOptions){
+          return this.filterBootCamps().map((option, key) => this.parseDetailsIntoJSX(option, key));
+        }
+      } catch(e) {
+        console.log("Error caught: " + e);
       }
-      
-      return allPrograms;
     }
-    
-    render() {
 
+    filterBootCamps(){
+      return this.state.cohortOptions.filter(option => option.name.toLowerCase().includes('bootcamp'));
+    }
+
+    parseDetailsIntoJSX(option, key){
+      const [, version, boot, camp] = /(\w\d*) (Boot)(Camp)/.exec(option.name);
+      const partTimeOrFull = option.name.includes('6 Month') ? "part-time session: " : "full-time session: ";
+      const startDate = `${new Date(option.startDate + 'EST').toDateString()}`;
+      const endDate = `${new Date(option.endDate + 'EST').toDateString()}`;
+      return (
+        <p key={key} className="currentCohortsP">
+          <span className="currentCohortsSpan">{`${version} ${boot} ${camp}, ${partTimeOrFull}`}</span> {startDate} - {endDate}
+        </p>
+      )
+    }
+
+    render() {
       return (
         <div className="currentCohorts">
-          {this.createTextForPage()}
+          {this.cohortDetailsJSX()}
         </div>
       )
     }
