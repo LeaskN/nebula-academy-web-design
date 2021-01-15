327d5715045af553785c2436ef2631719c984661
tefedrb-enterprise <tefebell@gmail.com>
2021-01-14 23:51:17 -0500

    removed footer hamburger & moved social media icons

--- software-engineering/src/components/Global/Footer.js
+++ software-engineering/src/components/Global/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 import { AiFillLinkedin, AiFillTwitterCircle, AiFillInstagram, AiFillFacebook, AiTwotoneMail, AiTwotonePhone} from 'react-icons/ai'
+import { IconContext } from 'react-icons';
 import NebulaIcon from '../../assets/NAIconTransparent240.png';
 import './Footer.css';
 
@@ -9,14 +10,36 @@ export const Footer = () => (
         <Navbar bg="dark" className="footerNavbar mainNavbar navbar-dark navbar-collapse"  style={{fontSize: '.80em', lineHeight:'10px'}}toggleable="true" expand="lg">
             <Container style={{ alignItems: 'flex-start', alignContent: 'flex-start', paddingTop: '15px'}}>
                 <Navbar.Brand href="/"><img src={ NebulaIcon } height="70px" alt=""></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav"/> */}
                 <Navbar.Collapse id="basic-navbar-nav" style={{ justifyContent: 'flex-start', alignItems: 'flex-start', alignContent: 'flex-start'}}>
-                <ul style={{justifyContent: 'flex-start', alignItems: 'flex-start', alignContent: 'flex-start', padding:'0px', whiteSpace: 'nowrap'}}>
-                    <h5 style={{color:'#BBBBBB'}}>Contact</h5>
-                    <Nav.Item><Nav.Link style={{textAlign:'left'}} href="tel:631-468-7477"><AiTwotonePhone/> 631-468-7477</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link style={{textAlign:'left'}} href="mailto:succeed@nebulaacademyny.com"><AiTwotoneMail/> succeed@nebulaacademyny.com</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link style={{textAlign:'left'}} href="https://www.google.com/maps/dir//180+Michael+Dr,+Syosset,+NY+11791,+USA/@40.8042056,-73.5511779,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x89c2817e5a652d8f:0x801ed763ddf8b24!2m2!1d-73.5161585!2d40.8042109!3e0" target="blank">180 Michael Drive Syosset, NY 11791</Nav.Link></Nav.Item>
-                </ul>
+                <div>
+                    <ul style={{justifyContent: 'flex-start', alignItems: 'flex-start', alignContent: 'flex-start', padding:'0px', whiteSpace: 'nowrap'}}>
+                        <h5 style={{color:'#BBBBBB'}}>Contact</h5>
+                        <Nav.Item><Nav.Link style={{textAlign:'left'}} href="tel:631-468-7477"><AiTwotonePhone/> 631-468-7477</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link style={{textAlign:'left'}} href="mailto:succeed@nebulaacademyny.com"><AiTwotoneMail/> succeed@nebulaacademyny.com</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link style={{textAlign:'left'}} href="https://www.google.com/maps/dir//180+Michael+Dr,+Syosset,+NY+11791,+USA/@40.8042056,-73.5511779,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x89c2817e5a652d8f:0x801ed763ddf8b24!2m2!1d-73.5161585!2d40.8042109!3e0" target="blank">180 Michael Drive Syosset, NY 11791</Nav.Link></Nav.Item>
+                    </ul>
+                    <Nav className="ml-auto customFooter">
+                        <IconContext.Provider value={{size: ".5em"}}>
+                            <Row>
+                                <Col>
+                                    <Nav.Item>
+                                        <Nav.Link target="blank" className="footerItem" href="https://www.facebook.com/NebulaAcademyNY/"><AiFillFacebook className="responsiveIcon"/></Nav.Link>
+                                    </Nav.Item>
+                                </Col>
+                                <Col>
+                                    <Nav.Item><Nav.Link target="blank" className="footerItem" href="https://www.linkedin.com/company/40795558/admin/"><AiFillLinkedin className="responsiveIcon"/></Nav.Link></Nav.Item>
+                                </Col>
+                                <Col>
+                                    <Nav.Item><Nav.Link target="blank" className="footerItem" href="https://www.instagram.com/nebulaacademia/"><AiFillInstagram className="responsiveIcon"/></Nav.Link></Nav.Item>
+                                </Col>
+                                <Col>
+                                    <Nav.Item><Nav.Link target="blank" className="footerItem" href="https://twitter.com/nynebula?lang=en"><AiFillTwitterCircle className="responsiveIcon"/></Nav.Link></Nav.Item>
+                                </Col>
+                            </Row>
+                        </IconContext.Provider>   
+                    </Nav>
+                </div>
                 <ul>
                     <h5 style={{color:'#BBBBBB'}}>All Links</h5>
                     <li className="noPadding noMargin nobull smallLineHeight"><Nav.Link className="footerItem" href="/">About</Nav.Link></li>
@@ -43,22 +66,6 @@ export const Footer = () => (
                     <Nav.Item><Nav.Link style={{textAlign:'left'}} href="https://www.youtube.com/watch?v=Ukg_U3CnJWI" target="blank" download>JavaScript in 12 mins</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link style={{textAlign:'left'}} href="https://www.youtube.com/playlist?list=PLM_i0obccy3uiITMEb7PUlzgIFlxq9fvv" target="blank" download>Tic Tac Toe Coding Challenge</Nav.Link></Nav.Item>
                 </ul>
-                    <Nav className="ml-auto customFooter">
-                        <Row>
-                            <Col>
-                                <Nav.Item><Nav.Link style={{padding: '0px'}} target="blank" className="footerItem" href="https://www.facebook.com/NebulaAcademyNY/"><AiFillFacebook /></Nav.Link></Nav.Item>
-                            </Col>
-                            <Col>
-                                <Nav.Item><Nav.Link style={{padding: '0px'}} target="blank" className="footerItem" href="https://www.linkedin.com/company/40795558/admin/"><AiFillLinkedin /></Nav.Link></Nav.Item>
-                            </Col>
-                            <Col>
-                                <Nav.Item><Nav.Link style={{padding: '0px'}} target="blank" className="footerItem" href="https://www.instagram.com/nebulaacademia/"><AiFillInstagram /></Nav.Link></Nav.Item>
-                            </Col>
-                            <Col>
-                                <Nav.Item><Nav.Link style={{padding: '0px'}} target="blank" className="footerItem" href="https://twitter.com/nynebula?lang=en"><AiFillTwitterCircle /></Nav.Link></Nav.Item>
-                            </Col>
-                        </Row>
-                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
