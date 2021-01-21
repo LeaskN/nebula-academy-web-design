d4ef0a93574ca43b09e0bf4bbe15bd0c79ada092
LeaskN <leask.nicholas@gmail.com>
2021-01-21 14:10:23 -0500

    updated version

--- software-engineering/src/App.js
+++ software-engineering/src/App.js
@@ -32,7 +32,7 @@ ReactGA.initialize('G-3H3KR3P2P1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
-  console.log('V:1.20.21');
+  console.log('V:1.21.21.GA');
   return (
     <React.Fragment>
       <CookieConsent
