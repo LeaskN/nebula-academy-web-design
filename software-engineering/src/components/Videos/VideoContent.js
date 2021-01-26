fff5a9a7ed9ec161204bbf48c71f4de902fe2cef
Manami Ueda <manamiueda1@gmail.com>
2021-01-26 16:21:31 -0500

    videos showing up properly

--- software-engineering/src/components/Videos/VideoContent.js
+++ software-engineering/src/components/Videos/VideoContent.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
-import './Videos.css'
+import './VideoContent.css'
 
 class VideoContent extends Component {
     constructor(props){
         super(props);
         this.state = {
             seV1Videos: [
-                
+                "https://player.vimeo.com/video/503214334",
+                "https://www.youtube.com/embed/W6NZfCO5SIk",
+                "https://www.youtube.com/embed/W6NZfCO5SIk"
             ],
             seV2Videos: [
 
@@ -17,8 +19,16 @@ class VideoContent extends Component {
     render(){
         return (
             <div>
-                <h1>Videos</h1>
-                <iframe src="https://www.youtube.com/embed/W6NZfCO5SIk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <h1></h1>
+                    <div id='videoList'>
+                        <div class ='videoGroup'>
+                            <h3>Software Engineering Bootcamp V1 Videos</h3>
+                            {this.state.seV1Videos.map(video => (
+                                <iframe src={video} width="300" height='auto' frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                             ))}
+                        </div>
+                    
+                    </div>
             </div>
         )
     }
