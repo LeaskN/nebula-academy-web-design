9e2c71cab4b3949b58abef77c48afb7ebd3cd92c
Manami Ueda <manamiueda1@gmail.com>
2021-02-05 12:47:34 -0500

    all jumbotrons are edited

--- NebulaAcademyHome/src/components/Universities/UniversitiesJumbotron.js
+++ NebulaAcademyHome/src/components/Universities/UniversitiesJumbotron.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Jumbotron, Container, Button } from 'react-bootstrap';
-import './UniversitiesJumbotron.css'
+import React from "react";
+import { Jumbotron, Container, Button } from "react-bootstrap";
+import "./UniversitiesJumbotron.css";
 
 export const UniversitiesJumbotron = () => (
   <React.Fragment>
     <Jumbotron className="noMargin UniversitiesJumbo">
       <Container>
-      <h1 className="contentCentered">Universities</h1>
-      <p>Nebula Academy bridges the gap between Academia and Industry. We partner with universities to remain on the cutting edge of higher education.</p>
-      <Button href="https://calendly.com/tomlemons/nebulaacademy" target="empty"  variant="secondary">Impact my University Today</Button>
+        <h1 className="contentCentered">Universities</h1>
+        <p>
+          Nebula Academy bridges the gap between Academia and Industry. We
+          partner with universities to remain on the cutting edge of higher
+          education.
+        </p>
+        <Button
+          href="https://calendly.com/tomlemons/nebulaacademy"
+          target="empty"
+          variant="secondary"
+        >
+          Impact my University Today
+        </Button>
       </Container>
     </Jumbotron>
   </React.Fragment>
-)
+);
