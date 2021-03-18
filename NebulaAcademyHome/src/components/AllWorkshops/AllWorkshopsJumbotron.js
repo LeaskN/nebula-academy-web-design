0839597e91d00eb126c62dafe7989725a14e5945
Nicholas Leask <leask.nicholas@gmail.com>
2021-03-18 11:10:12 -0400

    converted all wctd links to we-connect-the-dots links

--- NebulaAcademyHome/src/components/AllWorkshops/AllWorkshopsJumbotron.js
+++ NebulaAcademyHome/src/components/AllWorkshops/AllWorkshopsJumbotron.js
@@ -8,7 +8,7 @@ export const AllWorkshopsJumbotron = () => (
       <Container>
       <h1 className="contentCentered">AllWorkshops</h1>
       <p>Nebula Academy is a social enterprise institution. We aspire to help our community and world by giving back. Below are some programs from our not-for-profit, We Connect The Dots (<a href="https://we-connect-the-dots.org" className="link">We-Connect-The-Dots.org</a>), that you can help support by funding our scholarships, sponsoring, donating, or volunteering. With your support, we can help underserved and underrepresented communities with students, ages 7-18, and adults locally, nationally and globally, through our programs. </p>
-      <Button variant='info' href="https://wctd.org" target="blank">We Connect the Dots</Button>
+      <Button variant='info' href="https://we-connect-the-dots.org" target="blank">We Connect the Dots</Button>
       </Container>
     </Jumbotron>
   </React.Fragment>
