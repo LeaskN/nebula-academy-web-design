b4eaecc6f22f73a49ca924df2b65de2a199a6880
Nicholas Leask <leask.nicholas@gmail.com>
2021-03-22 11:53:41 -0400

    updated version id in app.js

--- NebulaAcademyHome/src/App.js
+++ NebulaAcademyHome/src/App.js
@@ -24,7 +24,7 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   render() {
-    console.log('V:3.18.21');
+    console.log('V:3.22.21-GA');
     return (
       <React.Fragment>
         <NavigationBar />
